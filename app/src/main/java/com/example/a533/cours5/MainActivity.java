8c5dd72354a98a7fe777a3bf9979ec7c9c67bdf6
B533P-04\533 <simoroy94@gmail.com>
2019-03-25 10:51:34 -0400

    Ajout des ... pour le choix du logout

--- app/src/main/java/com/example/a533/cours5/MainActivity.java
+++ app/src/main/java/com/example/a533/cours5/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a533.cours5;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,6 +13,24 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_log_out, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                //Write your code
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
